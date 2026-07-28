e5c35ba409f540371913e414343338c7f12528a4
aadya07tiwari <aadya07tiwari@gmail.com>
2026-07-28 22:47:17 +0530

    Added computer and human choice functions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+function getComputerChoice(){
+    let ranChoice=Math.random();
+    if(ranChoice==0) return "rock";
+    else if(ranChoice>0) return "paper";
+    else return "scissors";
+}
+console.log(getComputerChoice())
+function getHumanChoice(){
+    let humChoice=prompt("what do u want to choose? (rock/paper/scissors");
+}
+console.log(getHumanChoice())
